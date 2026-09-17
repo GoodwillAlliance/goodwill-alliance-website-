e4e0456de34fd6781cefae3df2538db6fcba26da
GoodwillAlliance <goodwillalliance@gmail.com>
2026-09-17 14:15:38 -0400

    Integrate Google Analytics into layout
    Added Google Analytics script for tracking.

--- goodwill-alliance-vercel-site-v3-2/app/layout.js
+++ goodwill-alliance-vercel-site-v3-2/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -22,7 +23,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-VS3KFNQ9JZ"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-VS3KFNQ9JZ');
+    `}
+  </Script>
+  {children}
+</body>
     </html>
   );
 }
